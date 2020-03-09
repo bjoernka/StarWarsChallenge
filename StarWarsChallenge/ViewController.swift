fd2e7b1c75be23a726a16a253d79becdcdad2b53
Bjoern Kaczmarek <bjoern.kaczmarek@gmail.com>
2020-03-09 15:54:35 +1100

    Initial Commit

---
+++ StarWarsChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsChallenge
+//
+//  Created by Björn Kaczmarek on 9/3/20.
+//  Copyright © 2020 Björn Kaczmarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
